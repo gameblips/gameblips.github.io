9984357ae8b929e4745088fe4a62902719f9b098
gameblips <gameblips@gmail.com>
2017-12-29 16:39:46 -0600

    Initial push
    First site

---
+++ index_htm_files/1.js
@@ -0,0 +1,38 @@
+window.local_simpleGallery3Columns_0_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "index_htm_files/131.jpg",
+ thumbnail: "index_htm_files/132.jpg"
+},
+{
+ image: "index_htm_files/133.jpg",
+ thumbnail: "index_htm_files/134.jpg"
+},
+{
+ image: "index_htm_files/135.jpg",
+ thumbnail: "index_htm_files/136.jpg"
+},
+{
+ image: "index_htm_files/137.jpg",
+ thumbnail: "index_htm_files/138.jpg"
+},
+{
+ image: "index_htm_files/139.jpg",
+ thumbnail: "index_htm_files/140.jpg"
+},
+{
+ image: "index_htm_files/141.jpg",
+ thumbnail: "index_htm_files/142.jpg"
+},
+{
+ image: "index_htm_files/143.jpg",
+ thumbnail: "index_htm_files/144.jpg"
+},
+{
+ image: "index_htm_files/145.jpg",
+ thumbnail: "index_htm_files/146.jpg"
+}
+];
---
+++ index_htm_files/2.js
@@ -0,0 +1,38 @@
+window.local_simpleGallery3Columns_1_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "index_htm_files/131.jpg",
+ thumbnail: "index_htm_files/148.jpg"
+},
+{
+ image: "index_htm_files/133.jpg",
+ thumbnail: "index_htm_files/149.jpg"
+},
+{
+ image: "index_htm_files/135.jpg",
+ thumbnail: "index_htm_files/150.jpg"
+},
+{
+ image: "index_htm_files/137.jpg",
+ thumbnail: "index_htm_files/151.jpg"
+},
+{
+ image: "index_htm_files/139.jpg",
+ thumbnail: "index_htm_files/152.jpg"
+},
+{
+ image: "index_htm_files/141.jpg",
+ thumbnail: "index_htm_files/153.jpg"
+},
+{
+ image: "index_htm_files/143.jpg",
+ thumbnail: "index_htm_files/154.jpg"
+},
+{
+ image: "index_htm_files/145.jpg",
+ thumbnail: "index_htm_files/155.jpg"
+}
+];
